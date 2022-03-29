c27a334c7a41c9737775191f4735aa35d4db4d46
Cesar Boaventura <cesar.boaventura61@gmail.com>
2022-03-29 17:47:25 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+speedlify circleci
---
+++ _data/sites/emcasa.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sample', // optional, falls back to object key
+  description: 'The default sites that get tested',
+  options: {
+    frequency: 0.1111111 / 60, // (in minutes), 23 hours
+  },
+  urls: ['https://www.emcasa.com/'],
+};
